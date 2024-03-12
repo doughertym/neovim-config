dbc935edfe093a48d318157c077840998e2d6f38
Mike Dougherty <miked@threedev.io>
2024-03-12 15:32:05 -0400

    refactor the structure for cleanliness

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
-
-vim.cmd [[
-  set expandtab
-  set tabstop=2
-  set softtabstop=2
-  set shiftwidth=2
-]]
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -20,22 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("settings")
 require("lazy").setup("plugins")
--- Telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
---vim.keymap.set('n', '<C-N>', ':Neotree filesystem close<CR>')
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  -- ensure_installed = {"lua", "java", "javascript", "c_sharp", "json"},
-  ensure_installed = {},
-  highlight = { enabled = true },
-  indent = { enabled = true }
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
---
+++ lua/settings.lua
@@ -0,0 +1,9 @@
+
+vim.cmd [[
+  set expandtab
+  set tabstop=2
+  set softtabstop=2
+  set shiftwidth=2
+]]
+vim.g.mapleader = " "
+
