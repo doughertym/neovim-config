f35fa11a036bad2ec5154c3aae3995e9a06300c9
Mike Dougherty <mike@corgibytes.com>
2024-03-15 19:39:49 -0400

    configure snippets and completions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,6 +21,11 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
 
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.clangd.setup({
+        capabilities = capabilities
+      })
+
       -- TODO add more key bindings?
       vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
