e28ff9ba54840cf422db26ee2691865cc0034b2e
Mike Dougherty <mike@corgibytes.com>
2024-03-17 17:37:38 -0400

    fix broken live_grep (SPC gr) command

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,8 @@ return {
     config = function()
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      -- Requires https://github.com/BurntSushi/ripgrep to be installed
+      vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
     end
   },
   {
